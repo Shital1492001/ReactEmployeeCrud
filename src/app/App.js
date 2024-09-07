5bede5b01dd3011a941730587446d8f67decf51b
Shital1492001 <124291113+Shital1492001@users.noreply.github.com>
2024-09-06 18:11:48 -0700

    Normalize all line endings

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import {Header} from '../header/Header';
+import './App.css';
+// <React.Fragment></React.Fragment>
+function App() {
+  //development :Js
+  return (
+    //Template: JsX
+    <>
+    <h1 className='mainHeading'>NEOSOFT EMPLOYEE MANAGEMENT SYSTEM</h1>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,8 @@
+import './Header.css'
+export function Header() {
+    return(
+        <header className="header">
+            <h4 className="subheading">Employee CRUD</h4>
+        </header>
+    );
+}
